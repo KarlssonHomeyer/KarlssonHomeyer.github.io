fc7f009dcbaeed8203298d748266f94cb708a6ba
KarlssonHomeyer <karlssonhomeyer1@gmail.com>
2023-10-11 13:46:24 +0200

    er laeuft und springt

--- Game/script.js
+++ Game/script.js
@@ -4,12 +4,15 @@ var block = document.getElementById("block");
 var hintergrund = document.getElementById("hintergrund");
 var counter=0;
 function jump(){
-    if(character.classList == "animate"){return}
-    character.classList.add("animate");
+    character.classList.remove("run");
+    if(character.classList == "jump"){return}
+    character.classList.add("jump");
     setTimeout(function(){
-        character.classList.remove("animate");
+        character.classList.remove("jump");
+        character.classList.add("run");
     },300);
 }
+
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
