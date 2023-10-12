8b198e2235e8bab73f16ed4fd1549ad012895c14
KarlssonHomeyer <karlssonhomeyer1@gmail.com>
2023-10-12 13:41:16 +0200

    spiel verschoenert

--- Game/script.js
+++ Game/script.js
@@ -3,6 +3,8 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 var hintergrund = document.getElementById("hintergrund");
 var counter=0;
+var game =document.getElementById("game");
+console.log("style:", document.getElementById('game').style.display);
 function jump(){
     character.classList.remove("run");
     if(character.classList == "jump"){return}
@@ -14,17 +16,57 @@ function jump(){
 }
 
 var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<120 && blockLeft>100 && characterTop>=130){
-        block.style.animation = "none";
-        hintergrund.style.animation = "none";
-        alert("Game over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1.575s infinite linear";
-        hintergrund.style.animation = "hintergrund 1.5s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+   
+    if(document.getElementById('game').style.display=="block")  {
+        let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+        if(blockLeft<120 && blockLeft>100 && characterTop>=130){
+            block.style.animation = "none";
+            hintergrund.style.animation = "none";
+            counter=0;
+            block.style.animation = "block 1.575s infinite linear";
+            hintergrund.style.animation = "hintergrund 1.5s infinite linear";
+            gameOver();
+        }else{
+            counter++;
+            document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        }
     }
-}, 10);
+    else {
+        score.style.display= "none"
+    }
+
+    }, 10);
+
+function startGame() {
+    let startDiv = document.getElementById("start");
+    let game = document.getElementById("game");
+    let gameOver = document.getElementById("game-over");
+    startDiv.style.display = "none";
+    game.style.display = "block";
+    gameOver.style.display = "none";
+    score.style.display = "block"
+
+}
+
+function gameOver() {
+    let startDiv = document.getElementById("start");
+    let game = document.getElementById("game");
+    let gameOver = document.getElementById("game-over");
+    startDiv.style.display = "none";
+    game.style.display = "none";
+    gameOver.style.display = "block";
+     
+}
+
+function launchIfReady() {
+    resourcesToLoad--;
+    if (resourcesToLoad == 0) {
+        let startDiv = document.getElementById("start");
+        let game = document.getElementById("game");
+        let gameOver = document.getElementById("game-over");
+        startDiv.style.display = "block";
+        game.style.display = "none";
+        gameOver.style.display = "none";
+    }
+}
